460c0ba5fe9261288e78e533a7c9fc4947dcbe25
briandgomez <briandgomez98@gmail.com>
2021-11-24 18:38:40 -0500

    server

---
+++ server.js
@@ -0,0 +1,45 @@
+//Helps connect to differrent files and directories easier
+const path = require('path');
+
+const express = require('express');
+//Creates a session
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+//Creates express application
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//Session requirements
+const sess = {
+    secret: 'secret password',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers')
+
+const hbs = exphbs.create({ helpers });
+
+//Uses handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'))
+});
